0aff03dd06b3e487cb936790218cb987f69196d3
mr.z <zsh2088@gmail.com>
2019-09-19 17:01:46 +0800

    fix api

--- src/Service/ApiService.php
+++ src/Service/ApiService.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Smart\Models\SysApiLog;
 
-class ApiService {
+class ApiService implements SmartService {
 
     const PARAM_REQUIRED = 'required';
     const PARAM_DIGIT    = 'digit';
